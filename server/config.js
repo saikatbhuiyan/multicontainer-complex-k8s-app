83e2f798f5be0c2d8892812771fc8e9999326dff
saikatbhuiyan <shaikotbhuiya@gmail.com>
2023-04-01 14:25:08 +0600

    Start from multicontainer app

---
+++ server/config.js
@@ -0,0 +1,11 @@
+console.log("process.env.POSTGRES_PASSWORD:", process.env.PGPASSWORD);
+
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT,
+};
